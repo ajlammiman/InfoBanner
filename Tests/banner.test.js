3c4ede550c0cdb8621f0b07c3b367708e44ced03
adam lammiman <ajlammiman@hotmail.com>
2018-10-19 21:56:15 +0100

    Added jest tests.

---
+++ Tests/banner.test.js
@@ -0,0 +1,21 @@
+  //          import React from "react";
+//import { shallow } from 'enzyme';
+//import Banner from './Components/Banner.jsx'
+
+/*describe('Test Banner Render', () => {
+    let props;
+    let wrapper;
+
+    beforeEach(() => {
+        props = {
+            title: 'My Title',
+            name: 'My Name'
+        };
+
+        wrapper = shallow(<Banner />);
+    });
+
+    it('renders component', () => {
+        expect(wrapper).toBeDefined();
+    });
+});*/
